19b11160b0d34ba512e2b517213b4d499eff735e
Donatas Noreika <donoras@gmail.com>
2023-04-28 11:37:38 +0300

    +Vieno produkto atvaizdavimas

--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def products(request):
@@ -8,3 +8,10 @@ def products(request):
         "products": Product.objects.all()
     }
     return render(request, "products.html", context=context)
+
+
+def product(request, product_id):
+    context = {
+        "product": get_object_or_404(Product, pk=product_id)
+    }
+    return render(request, 'product.html', context=context)
